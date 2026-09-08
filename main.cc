3fdc13e0ca17e108190b6b1dbb9c21c81a45761d
SaraHenriquezz <00160526>
2026-09-08 15:31:11 -0600

    feat: implementar lógica de consulta de stock

---
+++ main.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+void fConsultarStock(int stock);  
+
+
+int main(){ 
+    int stock = 50;
+
+
+
+
+    return 0;
+}
+
+void fConsultarStock(int stock)
+{
+    std::cout << "la cantidad de stock es: " << stock << std::endl;
+}
