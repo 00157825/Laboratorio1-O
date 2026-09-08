72c378b4ff8fe7204896053e2d04eddac429689b
00157825 <00157825@uca.edu.sv>
2026-09-08 14:32:20 -0600

    :sparkles: feat: formato al codigo

--- ingresar_stock.cc
+++ ingresar_stock.cc
@@ -4,18 +4,16 @@ void IngresarStock(int &stock, int cantidad)
 {
     if (cantidad < 0)
     {
-        std::cout << "No se permiten numeros negativos"<<std::endl;
+        std::cout << "No se permiten numeros negativos" << std::endl;
     }
 
     else
     {
         stock += cantidad;
-        std::cout << "Operación Exitosa. Se ingresaron: "<< cantidad<<" unidades"<<std::endl;
+        std::cout << "Operación Exitosa. Se ingresaron: " << cantidad << " unidades" << std::endl;
     }
-    
 }
 
 int main()
 {
-        
 }
