f343adfd8f624384f733890bd0f4cce4ba672dc1
00157825 <00157825@uca.edu.sv>
2026-09-08 15:20:48 -0600

    :sparkles: feat: integrar funcion de ingresar stock

---
+++ main.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void IngresarStock(int &stock, int cantidad)
+{
+    if (cantidad < 0)
+    {
+        std::cout << "No se permiten numeros negativos" << std::endl;
+    }
+
+    else
+    {
+        stock += cantidad;
+        std::cout << "Operación Exitosa. Se ingresaron: " << cantidad << " unidades" << std::endl;
+    }
+}
+
+int main()
+{
+    
+}
